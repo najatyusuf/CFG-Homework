00b736d71e5098fbc9453d99e2d2aa2f191ad78d
najatyusuf <najatyusuf01@hotmail.com>
2022-11-03 20:17:12 +0000

    Specialisation Homework: WEEK 4

---
+++ week4_homework.py
@@ -0,0 +1,45 @@
+def search_in_matrix(matrix, target, length):
+    """
+    :param matrix: matrix (list of lists)
+    :param target: target value to search in matrix
+    :param length: length of matrix
+    :return: coordinates of target value
+    """
+    i = 0
+    j = length - 1  # top right element
+
+    while i < length and j >= 0:
+
+        if matrix[i][j] == target:
+            return [i, j]
+        elif matrix[i][j] > target:
+            j -= 1
+        elif matrix[i][j] < target:
+            i += 1
+
+    return [-1, -1]
+
+
+matrx = [
+    [1, 4, 7, 12, 15, 1000],
+    [2, 5, 19, 31, 32, 1001],
+    [3, 8, 24, 33, 35, 1002],
+    [40, 41, 42, 44, 45, 1003],
+    [99, 100, 103, 106, 128, 1004]
+]
+
+targ = 44
+matrx_len = len(matrx[0])
+
+# returns target coordinates if in matrix
+print(search_in_matrix(matrx, targ, matrx_len))
+print(search_in_matrix(matrx, 1003, matrx_len))
+print(search_in_matrix(matrx, 33, matrx_len))
+print(search_in_matrix(matrx, 40, matrx_len))
+
+
+# returns [-1, -1] if not in matrix
+print(search_in_matrix(matrx, 72, matrx_len))
+print(search_in_matrix(matrx, 91, matrx_len))
+print(search_in_matrix(matrx, 27, matrx_len))
+print(search_in_matrix(matrx, 13, matrx_len))
